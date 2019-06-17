993854a3baad89c6ce5fd6d34e48ef10d449ceec
maixiaojie <778273234@qq.com>
2019-06-17 18:22:35 +0800

    聊天室界面优化

--- src/pages/chatroom/index.vue
+++ src/pages/chatroom/index.vue
@@ -5,27 +5,30 @@
           <image v-for="item in iconList" :key="item" :src="item"></image>
       </div>
     </div>
-    <div class="main">
-      <div class="row" v-for="item in msgList" :key="item.id" :class="item.uid === uid ? 'self': ''">       
-        <div v-if="item.type === 'notice'" class="notice">
-           <text >{{item.msg}} </text>
+    <div class="main" id="main">
+      <scroll-view scroll-y :style="{height: scrollHeight + 'rpx'}" :scroll-top="scrollTop">
+        <div class="row" v-for="item in msgList" :key="item.id" :class="item.uid === uid ? 'self': ''">       
+          <div v-if="item.type === 'notice'" class="notice">
+            <text >{{item.msg}} </text>
+          </div>
+          <div v-if="item.type === 'msg'" class="left">
+            <image :src="item.avator"></image>
+          </div>
+          <div v-if="item.type === 'msg'" class="right">
+            <div class="name" v-if="item.uid !== uid">{{item.name}}</div>
+            <div class="msg"><text>{{item.msg}}</text></div>
+          </div>
         </div>
-        <div v-if="item.type === 'msg'" class="left">
-          <image :src="item.avator"></image>
-        </div>
-        <div v-if="item.type === 'msg'" class="right">
-          <div class="name" v-if="item.uid !== uid">{{item.name}}</div>
-          <div class="msg"><text>{{item.msg}}</text></div>
-        </div>
-      </div>
+      </scroll-view>
+      
     </div>
     <div class="bottom">
       <div class="input_wrapper">
-         <input  class="input" @input="input" @confirm="confirm" :value="value" :cursor-spacing="20" confirm-type="send" type="text" placeholder="..." />
+         <input  class="input" @input="input" @confirm="confirm" :value="value" :cursor-spacing="20" confirm-type="send" type="text" placeholder="说点什么吧" />
       </div>
-      <div class="btn_wrapper">
+      <!-- <div class="btn_wrapper">
         <button class="btn" @click="confirm">发送</button>
-      </div>
+      </div> -->
       
     </div>
   </div>
@@ -39,6 +42,7 @@ export default {
     return {
       value: '',
       uid: '1',
+      scrollTop: 0,
       iconList: [
         'https://wx.qlogo.cn/mmopen/vi_32/3RN2u2ibTcKuTfXV25iacOUAZUd5oicgEbgcrFEfPRvoAs0QCWpeHewiar2PhwPnib86skZEnJqFrNZOMRI9qI6gYQA/132',
         'https://wx.qlogo.cn/mmopen/vi_32/s2RzxMKd4Dpx9HXia06XOpG6BMBgaia4VsqlB81kJShdT5VSg6aTxiboOmZkVdJMq3vKkJYZiaqqmBMLBd2BHwljzQ/132',
@@ -78,10 +82,17 @@ export default {
   components: {
   },
   watch: {
-    
+    'msgList.length'() {
+      this.scrollTop = this.msgList.length * 150; 
+    }
   },
   computed: {
-   
+    windowWidth() {
+      return globalStore.state.windowWidth
+    },
+   scrollHeight() {
+     return globalStore.state.windowHeight * (750 / this.windowWidth) - 120 - 100;
+   }
   },
   methods: {
     input(e) {
@@ -96,13 +107,16 @@ export default {
         avator: 'https://wx.qlogo.cn/mmopen/vi_32/3RN2u2ibTcKuTfXV25iacOUAZUd5oicgEbgcrFEfPRvoAs0QCWpeHewiar2PhwPnib86skZEnJqFrNZOMRI9qI6gYQA/132',
         msg
       }
-      console.log(data)
       this.msgList.push(data);
+      this.scrollTop = this.msgList.length * 150; 
     },
     confirm() {
       var msg = this.value;
-      this.sendMsg(msg);
-      this.value = '';
+      if(msg.length > 0) {
+        this.sendMsg(msg);
+        this.value = '';
+      }
+      
     }
     
   },
@@ -117,16 +131,23 @@ page {
   height: 100%;
 }
 .chatroom {
-  /* margin: 30rpx 30rpx; */
   width: 100%;
   height: 100%;
+  box-sizing: border-box;
   background: #F5F5F5;
   display: flex;
   flex-direction: column;
 }
 .chatroom .online {
-  height: 100rpx;
-  border-bottom: 1px solid rgb(190, 186, 186);
+  height: 99rpx;
+  z-index: 12;
+  left: 0;
+  top: 0;
+  position: fixed;
+  width: 100%;
+  background: #F5F5F5;
+  box-sizing: border-box;
+  border-bottom: 1rpx solid rgb(190, 186, 186);
 }
 .icon-list {
   padding:20rpx 50rpx;
@@ -143,12 +164,22 @@ page {
   border:1px solid white;
 }
 .chatroom .main {
+  width: 100%;
+  padding-top: 100rpx;
+  padding-bottom: 120rpx;
   flex: 1;
+  /* padding: 100rpx 0 120rpx 0; */
   background: #F5F5F5;
 }
 .chatroom .bottom {
-  height: 120rpx;
-  border-top: 1px solid rgb(190, 186, 186);
+  height: 119rpx;
+  z-index: 12;
+  left: 0;
+  bottom: 0;
+  position: fixed;
+  width: 100%;
+  box-sizing: border-box;
+  border-top: 1rpx solid rgb(190, 186, 186);
   display: flex;
   align-items:center;
   background: #F5F5F5;
@@ -158,24 +189,26 @@ page {
   background: #fff;
   border: 1rpx solid #F5F5F5;
   padding: 0 15rpx;
-  border-radius: 16rpx;
+  font-size: 28rpx;
+  border-radius: 5px;
 }
 .input_wrapper {
   flex: 1;
   box-sizing: border-box;
-  padding: 20rpx;
+  padding: 20rpx 20rpx 20rpx 20rpx;
 }
-.btn_wrapper {
+/* .btn_wrapper {
   width: 200rpx;
-  padding: 20rpx;
+  padding: 20rpx 20rpx 20rpx 10rpx;
 }
 .btn {
-  width: 170rpx;
+  width: 200rpx;
   height: 80rpx;
   background: #fff;
   line-height: 80rpx;
   font-size: 30rpx;
-}
+  border:none;
+} */
 .chatroom .main .row {
   display: flex;
   padding: 20rpx;
